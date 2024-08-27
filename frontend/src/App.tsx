9e4e085bfa78a338f64666d5bc88a7ac0281ee05
Abdullah Ismail <abdullahmusa2003@gmail.com>
2024-08-27 11:08:27 +0100

    setup backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Layout>
+							<p>Home Page</p>
+						</Layout>
+					}
+				/>
+				<Route
+					path="/search"
+					element={
+						<Layout>
+							<p>Search Page</p>
+						</Layout>
+					}
+				/>
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
