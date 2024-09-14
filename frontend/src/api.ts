6a38f1346b89e7442a85ece62c1643dde796ba96
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-09-14 14:51:42 +0100

    created frontend api to check if user is logged in

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,17 +3,32 @@ import { RegisterFormData } from "./Pages/Register";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const postNewUser = async (newUserObject: RegisterFormData) => {
-		const response = await fetch(`${API_BASE_URL}/api/user/register`, {
-			method: "POST",
-			credentials: "include",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(newUserObject),
-		});
+	const response = await fetch(`${API_BASE_URL}/api/user/register`, {
+		method: "POST",
+		credentials: "include",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(newUserObject),
+	});
 
-		const responseBody = await response.json();
+	const responseBody = await response.json();
 
-		if (!response.ok) {
-			throw new Error(responseBody.message);
-		}
-		return responseBody;
-	} 
+	if (!response.ok) {
+		throw new Error(responseBody.message);
+	}
+	return responseBody;
+};
+
+export const validateToken = async () => {
+	const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+		credentials: "include"
+	})
+
+	const responseBody = await response.json()
+
+	if(!response.ok) {
+		throw new Error('Token invalid')
+	}
+
+	return responseBody
+}
+
