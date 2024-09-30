7984dd31f46ded9f68a3328c5a82f952b6ba659d
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-09-30 15:15:22 +0100

    created signoutbutton component with relevant functionality

---
+++ frontend/src/Components/SignOutButton.tsx
@@ -0,0 +1,41 @@
+import { signOut } from "../api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAppContext } from "../Contexts/useAppContext";
+import { useNavigate } from "react-router-dom";
+
+export default function SignOutButton() {
+	
+	const queryClient = useQueryClient();
+	const { showToast } = useAppContext();
+	const navigate = useNavigate();
+
+	const mutation = useMutation({
+		mutationFn: signOut,
+		onSuccess: async () => {
+			await queryClient.invalidateQueries({ queryKey: ["validateToken"] });
+			showToast({
+				message: "Sign Out Successful",
+				type: "SUCCESS",
+			});
+			navigate("/");
+		},
+		onError: (error: Error) => {
+			showToast({
+				message: error.message,
+				type: "ERROR",
+			});
+		},
+	});
+
+	const handleClick = () => {
+		mutation.mutate();
+	};
+	return (
+		<button
+			className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-100"
+			onClick={handleClick}
+		>
+			Sign Out
+		</button>
+	);
+}
