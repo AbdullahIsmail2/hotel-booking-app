85453a5dccad6740a8eb053d068ebc25dc8b9c38
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-09-30 15:14:13 +0100

    fixed hanging logout endpoint

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -67,6 +67,8 @@ router.post("/logout", (req: Request, res: Response) => {
 	res.cookie("auth_token", "", {
 		expires: new Date(0),
 	});
+	res.send()
+	// we created an empty token that expires at the time of creation
 });
 
 export default router;
