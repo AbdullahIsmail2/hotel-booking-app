d116bb9697bc1cbdf43d96a3f2f48e48bdaddec0
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-03 14:59:22 +0100

    added connention to e2e test db with its own script to connect to the test db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 
 mongoose
 	.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -13,7 +13,7 @@ mongoose
 
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
