9dfdb235077b9de479ca638a3a48ddb430c93686
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-10 11:30:14 +0100

    added all different sections to add my hotel form and added api function to allow ne hotel to be saved in mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import { v2 as cloudinary } from "cloudinary";
 import myHotelRoutes from "./routes/my-hotels";
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINAY_CLOUD_NAME,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import { RegisterFormData } from "./Pages/Register";
 import { SignInFormType } from "./Pages/SignIn";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 // api request for registering new user
 export const postNewUser = async (newUserObject: RegisterFormData) => {
@@ -39,22 +39,17 @@ export const signIn = async (userObject: SignInFormType) => {
 	return responseBody;
 };
 
-
-// api request for signing out 
-export  const signOut = async () => {
+// api request for signing out
+export const signOut = async () => {
 	const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
 		method: "POST",
 		credentials: "include",
 	});
 
-	if(!response.ok) {
-		throw new Error('error during sign out')
+	if (!response.ok) {
+		throw new Error("error during sign out");
 	}
-
-
-
 };
-
 
 // api request for validating token
 export const validateToken = async () => {
@@ -71,3 +66,16 @@ export const validateToken = async () => {
 	return responseBody;
 };
 
+export const addMyHotel = async (hotelFormData: FormData) => {
+	const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+		method: "POST",
+		credentials: "include",
+		body: hotelFormData,
+	});
+
+	if (!response.ok) {
+		throw new Error("Failed to add hotel");
+	}
+	
+	return response.json();
+};
