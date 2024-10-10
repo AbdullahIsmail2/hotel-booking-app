0ae659b3083a863b7834f63bef04ebe36f8a2de3
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-10 11:30:14 +0100

    added type section of add my hotel form

--- frontend/src/Pages/AddHotel.tsx
+++ frontend/src/Pages/AddHotel.tsx
@@ -1,5 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
 import ManageHotelForm from "../forms/ManageHotelForm";
+import { useAppContext } from "../Contexts/useAppContext";
+import { addMyHotel } from "../api";
 
 export default function AddHotel() {
-	return <ManageHotelForm />;
+	const { showToast } = useAppContext();
+	const { mutate, isPending } = useMutation({
+		mutationFn: addMyHotel,
+		onSuccess: () => {
+			showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+		},
+		onError: () => {
+			showToast({ message: "Error Saving Hotel", type: "ERROR" });
+		},
+	});
+
+	const handleSave = (hotelFormData: FormData) => {
+		mutate(hotelFormData);
+	};
+	return <ManageHotelForm onSave={handleSave} isPending={isPending} />;
 }
