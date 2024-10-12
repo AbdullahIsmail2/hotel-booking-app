74937fab82b87d744e5f8087898059b952551ea0
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-12 18:24:38 +0100

    add a catch all request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,6 +38,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
+app.get("*", (req: Request, res:Response) => {
+	res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+
+})
+
 app.listen(7000, () => {
 	console.log("server is running on localhost:7000");
 });
