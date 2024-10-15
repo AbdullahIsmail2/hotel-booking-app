de315bea21a5fd0bf5cd1610d5a36b90cfd1fff9
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-15 12:19:48 +0100

    added get hotel by idand update hotel by id endpoints

--- frontend/src/Pages/EditHotel.tsx
+++ frontend/src/Pages/EditHotel.tsx
@@ -1,15 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { fetchMyHotelById } from "../api";
+import { fetchMyHotelById, updateMyHotelById } from "../api";
 import ManageHotelForm from "../forms/ManageHotelForm";
+import { useAppContext } from "../Contexts/useAppContext";
 
 export default function EditHotel() {
 	const { hotelId } = useParams();
+	const { showToast } = useAppContext();
 
 	const { data: hotel } = useQuery({
 		queryKey: ["fetchMyHotelById"],
 		queryFn: () => fetchMyHotelById(hotelId || ""),
 		enabled: !!hotelId, // only run query if hotelId is truthy
 	});
-	return <ManageHotelForm hotel={hotel} />;
+
+	const { mutate, isPending } = useMutation({
+		mutationFn: updateMyHotelById,
+		onSuccess: () => {
+			showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+		},
+		onError: () => {
+			showToast({ message: "Error Saving Hotel", type: "ERROR" });
+		},
+	});
+
+	const handleSave = (hotelFormData: FormData) => {
+		mutate(hotelFormData);
+	};
+	return (
+		<ManageHotelForm hotel={hotel} onSave={handleSave} isPending={isPending} />
+	);
 }
--- frontend/src/forms/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm.tsx
@@ -17,29 +17,33 @@ export type HotelFormData = {
 	starRating: number;
 	facilities: string[];
 	imageFiles: FileList;
-	imageUrls: string[]
+	imageUrls: string[];
 	adultCount: number;
 	childCount: number;
 };
 
 type Props = {
-	hotel: HotelType
 	onSave: (hotelFormData: FormData) => void;
 	isPending: boolean;
+	hotel?: HotelType;
 };
 const ManageHotelForm = ({ hotel, onSave, isPending }: Props) => {
 	const formMethods = useForm<HotelFormData>();
 	const { handleSubmit, reset } = formMethods;
 
 	useEffect(() => {
-		reset(hotel)
-	}, [hotel, reset] )
+		reset(hotel);
+	}, [hotel, reset]);
 
 	const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
 		// because we have images in our data, we cannot send it as type json to backend
 		const formData = new FormData();
+
+		if (hotel) {
+			formData.append("hotelId", hotel._id);
+		}
 		formData.append("name", formDataJson.name);
-		formData.append("city", formDataJson.city);
+		formData.append("city", formDataJson.city); 
 		formData.append("country", formDataJson.country);
 		formData.append("description", formDataJson.description);
 		formData.append("type", formDataJson.type);
@@ -51,6 +55,12 @@ const ManageHotelForm = ({ hotel, onSave, isPending }: Props) => {
 		formDataJson.facilities.forEach((facility, index) => {
 			formData.append(`facilities[${index}]`, facility);
 		});
+
+		if(formDataJson.imageUrls) {
+			formDataJson.imageUrls.forEach((url, index) => {
+				formData.append(`imageUrls[${index}]`, url)
+			})
+		}
 
 		Array.from(formDataJson.imageFiles).forEach((imageFile) => {
 			formData.append(`imageFiles`, imageFile);
