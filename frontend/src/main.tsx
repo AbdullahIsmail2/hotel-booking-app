16d72dcd2e571108850cf60c24404866d56a175e
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-10-16 10:25:05 +0100

    added search context which will hold all search variables in global context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppContextProvider } from "./Contexts/AppContextProvider.tsx";
+import { SearchContextProvider } from "./Contexts/SearchContext.tsx";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -17,7 +18,9 @@ createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<AppContextProvider>
-				<App />
+				<SearchContextProvider>
+					<App />
+				</SearchContextProvider>
 			</AppContextProvider>
 		</QueryClientProvider>
 	</StrictMode>
